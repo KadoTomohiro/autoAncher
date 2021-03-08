18d04081621d1610d9f6817a792193a48659eb47
KadoTomohiro <>
2021-03-08 12:01:21 +0900

    Initial commit

---
+++ components/autoAnchor.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "autoAnchor"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <auto-anchor :text="text"></auto-anchor>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      text: `弊社HPはこちら
+      http://example.com
+      お問い合わせはこちら
+      https://example.com/callcenter
+      これは危険なURL
+      https://example.com/denger" onclick="alert(\\'hi\\')`,
+    }
+  },
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
