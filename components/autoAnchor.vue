e7e5397dbfba06d9fa84561d9f4e2d9d0e5352ed
KadoTomohiro <>
2021-03-08 12:02:57 +0900

    add autoAncher Component

--- components/autoAnchor.vue
+++ components/autoAnchor.vue
@@ -1,13 +1,37 @@
 <template>
-$END$
+  <p>
+    <template v-for="fragment in textFragments">
+      <a v-if="isUrl(fragment)" :href="fragment">{{ fragment }}</a>
+      <template v-else>{{ fragment }}</template>
+    </template>
+  </p>
 </template>
 
-<script>
-export default {
-name: "autoAnchor"
-}
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'AutoAnchor',
+  props: {
+    text: { type: String, default: '' },
+  },
+  data() {
+    return {
+      textFragments: [],
+      urlPattern: /(https?:\/\/[\w!?\/\+\-_~=;\.,*&@#$%\(\)\'\[\]]+)/i,
+    }
+  },
+  mounted() {
+    this.text
+      .split(this.urlPattern)
+      .forEach((textFragment) => this.textFragments.push(textFragment))
+  },
+  methods: {
+    isUrl(text: string): boolean {
+      return this.urlPattern.test(text)
+    },
+  },
+})
 </script>
 
-<style scoped>
-
-</style>
+<!--<style scoped></style>-->
---
+++ plugins/autoAnchorDirective.ts
@@ -0,0 +1,7 @@
+// import Vue from 'vue'
+//
+// Vue.directive('auto-anchor', {
+//   bind(el, binding, vnode) {
+//     const text: string = el.textContent
+//   },
+// })
